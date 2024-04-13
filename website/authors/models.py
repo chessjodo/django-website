425098e759adca3d5a12813aff5ee89ccbfdf864
Andrei Marculesteanu <andreimarculesteanu28@gmail.com>
2024-04-13 21:00:24 +0200

    created author_articles model

--- website/authors/models.py
+++ website/authors/models.py
@@ -1,10 +1,16 @@
+from articles.models import Article
 from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     bio_text = models.CharField(max_length=200)
     bio_edit = models.DateTimeField("info date edited")
+
+
+class Author_Articles(models.Model):
+    author_id = models.OneToOneField(Author, on_delete=models.CASCADE)
+    article_id = models.OneToOneField(Article, on_delete=models.CASCADE)
 
 
 # Create your models here.
