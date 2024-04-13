82175d2af615b5d387f8357db5ea2bfa8eb76a37
Andrei Marculesteanu <andreimarculesteanu28@gmail.com>
2024-04-13 21:06:10 +0200

    made author_articles visible in admin

--- website/authors/admin.py
+++ website/authors/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Author
+from .models import Author, Author_Articles
 
 admin.site.register(Author)
+admin.site.register(Author_Articles)
 # Register your models here.
