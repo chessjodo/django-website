bcc4b842d4026f99859dc427484b0051159f9a31
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-04-13 12:29:05 +0200

    add models to admin and create superuser

--- website/articles/admin.py
+++ website/articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+models = (Article, Comment, Tag, Article_Tag)
+for m in models:
+    admin.site.register(m)
