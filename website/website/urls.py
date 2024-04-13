7b074d131086eac7f687ab9653f4b19103d23188
Tom Heyning <tom.heyning@gmail.com>
2024-04-13 13:15:16 +0200

    set home page to index view

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", home_view, name="home"),
     path("admin/", admin.site.urls),
     path("articles/", include("articles.urls")),
 ]
