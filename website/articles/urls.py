a1587ec6b7083bd6a8c44f7f17a44bd6de76d4a2
Tom Heyning <tom.heyning@gmail.com>
2024-04-15 19:26:19 +0200

    add name to articles urls

--- website/articles/urls.py
+++ website/articles/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "articles"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:article_id>", views.article, name="article"),
-    path("<int:article_id>/comment/", views.comment, name="comment")
+    path("<int:article_id>/comment/", views.comment, name="comment"),
+    path("sort/", views.sort, name="sort"),
+    path("filter/", views.filter, name="filter")
 ]
