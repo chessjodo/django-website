d4ab9efa1750341eafa2dddce66ffb095699587e
Tom <tom.heyning@gmail.com>
2024-04-15 14:50:27 +0200

    add commenting functionality

--- website/articles/views.py
+++ website/articles/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from datetime import datetime
+from django.urls import reverse
 
 from .models import Article, Article_Tag, Comment, Tag
 
@@ -19,4 +21,12 @@ def article(request, article_id):
     return render(request, "articles/article.html", {"article": article})
 
 def comment(request, article_id):
-    return HttpResponse(f"you commented on article {article_id}")
+    article = get_object_or_404(Article, pk=article_id)
+    comment = request.POST["comment"]
+    c = Comment(article_ID=article, heading=request.POST["heading"], text=request.POST["comment"], 
+                pub_date=datetime.now())
+    c.save()
+
+    comments = article.comment_set.all()
+
+    return HttpResponseRedirect(reverse("article", args=(article.id,)))
