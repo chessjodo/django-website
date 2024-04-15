ac5682ceb94c57369fd325dd7823b62e416fb381
Tom Heyning <tom.heyning@gmail.com>
2024-04-15 21:06:34 +0200

    add option to filter articles, filtering does not fully work yet

--- website/articles/views.py
+++ website/articles/views.py
@@ -5,38 +5,49 @@ from django.urls import reverse
 
 from .models import Article, Article_Tag, Comment, Tag
 
-
 def index(request):
+    filt, filts = filterer(request)
+    articles, sort, sort_options = sorter(request)
+
+    context = {
+        "articles_list": articles, 
+        "sort_order": sort, 
+        "sort_options": sort_options,
+        "filt": filt,
+        "filters": filts
+    }
+    
+    return render(request, "articles/index.html", context)
+
+def sorter(request):
     if order:=request.GET.get('article-sort'):
         sort = order
-        if order == "newest":
+        if order == "Newest":
             articles = Article.objects.order_by("-pub_date")
-        elif order == "oldest":
+        elif order == "Oldest":
             articles = Article.objects.order_by("pub_date")
-        elif order == "A":
+        elif order == "A-Z":
             articles = Article.objects.order_by("heading")
-        elif order == "Z":
+        elif order == "Z-A":
             articles = Article.objects.order_by("-heading")
-        elif order == "most":
+        elif order == "Most Views":
             articles = Article.objects.order_by("-view_count")
         else:
             articles = Article.objects.order_by("view_count")
     else:
         articles = Article.objects.order_by("-pub_date")
-        sort = "newest"
+        sort = "Newest"
 
-    sort_options = (
-        ('newest', 'Newest'),
-        ('oldest', 'Oldest'),
-        ('A', 'A-Z'),
-        ('Z', 'Z-A'),
-        ('most', 'Most Views'),
-        ('least', 'Least Views'),
-    )
+    sort_options = ('Newest', 'Oldest', 'A-Z', 'Z-A', 'Most Views', 'Least Views')
 
-    context = {"articles_list": articles, "sort_order": sort, "sort_options": sort_options}
-    return render(request, "articles/index.html", context)
+    return (articles, sort, sort_options)
 
+def filterer(request):
+    filter_options = Tag.objects.all()
+    
+    filt = request.GET.get('article-filter')
+
+    return (filt, filter_options)
 
 def home_view(request):
     return render(request, "home.html", {})
