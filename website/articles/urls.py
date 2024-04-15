85ec82a0b561f3c9cd3e851b79a6e2871a011644
Tom Heyning <tom.heyning@gmail.com>
2024-04-15 20:17:54 +0200

    add ability to sort articles

--- website/articles/urls.py
+++ website/articles/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:article_id>", views.article, name="article"),
     path("<int:article_id>/comment/", views.comment, name="comment"),
-    path("sort/", views.sort, name="sort"),
-    path("filter/", views.filter, name="filter")
 ]
--- website/articles/views.py
+++ website/articles/views.py
@@ -7,8 +7,34 @@ from .models import Article, Article_Tag, Comment, Tag
 
 
 def index(request):
-    articles = Article.objects.order_by("-pub_date")
-    context = {"articles_list": articles}
+    if order:=request.GET.get('article-sort'):
+        sort = order
+        if order == "newest":
+            articles = Article.objects.order_by("-pub_date")
+        elif order == "oldest":
+            articles = Article.objects.order_by("pub_date")
+        elif order == "A":
+            articles = Article.objects.order_by("heading")
+        elif order == "Z":
+            articles = Article.objects.order_by("-heading")
+        elif order == "most":
+            articles = Article.objects.order_by("-view_count")
+        else:
+            articles = Article.objects.order_by("view_count")
+    else:
+        articles = Article.objects.order_by("-pub_date")
+        sort = "newest"
+
+    sort_options = (
+        ('newest', 'Newest'),
+        ('oldest', 'Oldest'),
+        ('A', 'A-Z'),
+        ('Z', 'Z-A'),
+        ('most', 'Most Views'),
+        ('least', 'Least Views'),
+    )
+
+    context = {"articles_list": articles, "sort_order": sort, "sort_options": sort_options}
     return render(request, "articles/index.html", context)
 
 
@@ -31,4 +57,4 @@ def comment(request, article_id):
     c = Comment(article_ID=article, heading=heading, text=comment, pub_date=datetime.now())
     c.save()
 
-    return HttpResponseRedirect(reverse("article", args=(article.id,)))
+    return HttpResponseRedirect(reverse("articles:article", args=(article.id,)))
