31f502e81bae86a2d8874247f8e2bd4def0ad792
Tom Heyning <tom.heyning@gmail.com>
2024-04-17 16:09:02 +0200

    add comments to templates and forms.py

--- website/articles/forms.py
+++ website/articles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class CommentForm(forms.Form):
+    """Form to handle commenting"""
     heading = forms.CharField(max_length=150)
     comment = forms.CharField(widget=forms.Textarea(attrs={"rows":"2", "cols":"30"}))
 
